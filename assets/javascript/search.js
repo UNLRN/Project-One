195419a14ae4e8659785c9b50a5daeeb915a4c20
Matt Jiles <cjiles@siibrands.com>
2017-06-20 14:27:05 -0400

    relink app.js to index

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -8,34 +8,34 @@ $('#overlay-search-close').on('click', function(){
     $('#overlay-search-input').val('');
 });
 
-const spotifyApp = {};
-const credentials = btoa('8f01408378c5438096f5f11e50a9d395'+':'+'cb37a328aaa84304aefdbe043f993412');
+// const spotifyApp = {};
+// const credentials = btoa('8f01408378c5438096f5f11e50a9d395'+':'+'cb37a328aaa84304aefdbe043f993412');
 
-spotifyApp.apiUrl = 'https://accounts.spotify.com/api/token';
+// spotifyApp.apiUrl = 'https://accounts.spotify.com/api/token';
 
-let myHeaders = new Headers();
-myHeaders.append("Authorization", "Basic " + credentials);
-myHeaders.append("Access-Control-Allow-Origin", "https://unlrn.github.io/");
+// let myHeaders = new Headers();
+// myHeaders.append("Authorization", "Basic " + credentials);
+// myHeaders.append("Access-Control-Allow-Origin", "https://unlrn.github.io/");
 
-let params = new URLSearchParams();
-params.append('grant_type', 'client_credentials');
+// let params = new URLSearchParams();
+// params.append('grant_type', 'client_credentials');
 
-let spotifyReq = new Request(spotifyApp.apiUrl, {
-    method: 'POST',
-    mode: 'cors',
-    headers: myHeaders,
-    body: params
-});
-
-
-spotifyApp.searchArtist = () => fetch(spotifyReq)
-    .then(blob => blob.json())
-    .then(data => console.log(data));
+// let spotifyReq = new Request(spotifyApp.apiUrl, {
+//     method: 'POST',
+//     mode: 'cors',
+//     headers: myHeaders,
+//     body: params
+// });
 
 
-spotifyApp.init = function() {
-    // spotifyApp.events();
-    spotifyApp.searchArtist();
-};
+// spotifyApp.searchArtist = () => fetch(spotifyReq)
+//     .then(blob => blob.json())
+//     .then(data => console.log(data));
 
-$(spotifyApp.init);
+
+// spotifyApp.init = function() {
+//     // spotifyApp.events();
+//     spotifyApp.searchArtist();
+// };
+
+// $(spotifyApp.init);
