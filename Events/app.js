05bb354c77c8ed7a59c3b3e8367eb53a4b919f93
Jessie Crow <jrmcrow@gmail.com>
2017-06-24 13:06:19 -0400

    event table

---
+++ Events/app.js
@@ -0,0 +1,30 @@
+var artist = $("#arist").val();
+
+$("#submit").on('click', function(e){
+    e.preventDefault();
+
+var artist = $("#artist").val();
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://rest.bandsintown.com/artists/' + artist + '/events?app_id=bootcamp',
+    }).then(function(data) {
+        console.log(data);
+
+        // for (var i = 0; i < data.length; i++) {
+        //     console.log(data[i].venue.name);
+        //     var div = $("<li>");
+        //     $(div).text(data[i].venue.name);
+        //     $(div).appendTo("#events");
+        // }
+        // for (var i = 0; i < data.length; i++) {
+        //     console.log(data[i].datetime);
+        //     var div = $("<li>");
+        //     $(div).text(data[i].datetime);
+        //     $(div).appendTo("#events");
+        // }
+        for (var i = 0; i < data.length; i++){
+            $('#event-table > tbody').append("<tr><td>" + data[i].venue.name + "</td><td>" + data[i].datetime + "</td><td>" + data[i].venue.city + "</td><td>" + data[i].venue.region + "</td><td>" + data[i].venue.country + "</td></tr>"
+            )};
+    });
+});
