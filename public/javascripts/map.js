22ae1bff01ec70a75575bc703fdeca9eecb23230
Matt Jiles <mjiles1@gmail.com>
2017-07-07 21:25:07 -0400

    cleanup

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -386,45 +386,6 @@ function initialize() {
     // Display a map on the page
     map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    // // Info Window Content
-    // var infoWindowContent = [
-    //     ['<div class="info_content">' +
-    //         '<h3>London Eye</h3>' +
-    //         '<p>The London Eye is a giant Ferris wheel situated on the banks of the River Thames. The entire structure is 135 metres (443 ft) tall and the wheel has a diameter of 120 metres (394 ft).</p>' + '</div>'
-    //     ]
-    // ];
-
-    // // Display multiple markers on a map
-    // var infoWindow = new google.maps.InfoWindow(),
-    //     marker, i;
-
-    // // Loop through our array of markers & place each one on the map  
-    // for (i = 0; i < markers.length; i++) {
-    //     var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
-    //     bounds.extend(position);
-    //     marker = new google.maps.Marker({
-    //         position: position,
-    //         map: map,
-    //         title: markers[i][0]
-    //     });
-
-    //     // Allow each marker to have an info window    
-    //     google.maps.event.addListener(marker, 'click', (function (marker, i) {
-    //         return function () {
-    //             infoWindow.setContent(infoWindowContent[i][0]);
-    //             infoWindow.open(map, marker);
-    //         }
-    //     })(marker, i));
-
-    //     // Automatically center the map fitting all markers on the screen
-    //     map.fitBounds(bounds);
-    // }
-
-    // // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-    // var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function (event) {
-    //     this.setZoom(14);
-    //     google.maps.event.removeListener(boundsListener);
-    // });
 }
 initialize();
 
